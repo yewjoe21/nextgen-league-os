3c7a68092728b27c46042d63694699961f52f2e0
yewjoe21 <yewjoe21@gmail.com>
2026-05-21 19:32:54 +0800

    v2

---
+++ page.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main style={{
+      background:"#020402",
+      minHeight:"100vh",
+      color:"white",
+      padding:"40px",
+      fontFamily:"Arial"
+    }}>
+      <h1 style={{
+        color:"#caff00",
+        fontSize:"60px",
+        fontWeight:"900"
+      }}>
+        NEXTGEN LEAGUE OS
+      </h1>
+
+      <p>System Ready</p>
+    </main>
+  )
+}
