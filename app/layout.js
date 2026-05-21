220160b68ab03f9b86ceb120c782268fec2e26ab
yewjoe21 <yewjoe21@gmail.com>
2026-05-21 20:04:56 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "NextGen League OS",
+  description: "Snooker league system"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  )
+}
