6135c1959c724a1a3ae1dfa514c79edc28129fbd
yewjoe21 <yewjoe21@gmail.com>
2026-05-21 19:57:55 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-export default function Home() {
-  return (
-    <main style={{
-      background:"#020402",
-      minHeight:"100vh",
-      color:"white",
-      padding:"40px",
-      fontFamily:"Arial"
-    }}>
-      <h1 style={{
-        color:"#caff00",
-        fontSize:"60px",
-        fontWeight:"900"
-      }}>
-        NEXTGEN LEAGUE OS
-      </h1>
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-      <p>System Ready</p>
-    </main>
-  )
-}
+module.exports = nextConfig
